ac8cb7bf964db65070293e7f52c417728b252641
Cyprien Thao <cyprienthao@gmail.com>
2020-04-02 11:54:29 +0200

    Update candidateProfile.vue

--- src/views/candidateProfile.vue
+++ src/views/candidateProfile.vue
@@ -182,7 +182,7 @@
                         <span>
                             <ul>
                                 <li>
-                                    <p class="text-primary">Secure code : </p>{{assess.key_token}}
+                                    <p class="text-primary">Secure code : </p>{{assess.secure_code}}
                                 </li>                                
                                 <li>
                                     <p class="text-primary">Candidate mail : </p>{{assess.email}}
